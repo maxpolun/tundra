0dd2be0586167de308891a6568a661afdaf9b3bc
Max Polun <orrerey@gmail.com>
2012-02-17 11:55:51 -0500

    initial lexer done! ints, +, -, and parens implemented

--- lex/lex.go
+++ lex/lex.go
@@ -1,18 +1,18 @@
 package lex
 
 import (
-//	"io"
-//	"unicode"
+	//"fmt"
+	"unicode"
+	"utf8"
 )
 
 const (
-	T_INT =  iota
+	T_INT = iota
 	T_PLUS
 	T_MINUS
 	T_LPAREN
 	T_RPAREN
 )
-
 
 const eof = -1
 
@@ -21,10 +21,10 @@ type Token struct {
 	Text  string
 }
 
-var tokenValues = map[int]string {
-	T_INT: "T_INT",
-	T_PLUS: "T_PLUS",
-	T_MINUS: "T_MINUS",
+var tokenValues = map[int]string{
+	T_INT:    "T_INT",
+	T_PLUS:   "T_PLUS",
+	T_MINUS:  "T_MINUS",
 	T_LPAREN: "T_LPAREN",
 	T_RPAREN: "T_RPAREN",
 }
@@ -45,11 +45,10 @@ type Lexer struct {
 type stateFn func(*Lexer) stateFn
 
 func NewLexer(s string) (*Lexer, chan Token) {
-	l := &Lexer{
-		
-	}
+	l := &Lexer{}
 	l.input = s
 	l.tokens = make(chan Token)
+	l.pos = 0
 	go l.run()
 
 	return l, l.tokens
@@ -58,32 +57,69 @@ func NewLexer(s string) (*Lexer, chan Token) {
 
 func (l *Lexer) run() {
 	/*
-	for state := defaultState; state != nil; {
-		state = state(l)
-	}
+		for state := defaultState; state != nil; {
+			state = state(l)
+		}
 	*/
-	l.tokens <- Token{Value:eof}
+
+	for rune := l.next(); rune != eof; rune = l.next() {
+		l.emitSingle(rune)
+	}
+	l.emit(eof)
 	close(l.tokens)
 }
-func emit(l *Lexer, value int) {
-	l.tokens <- Token{Value: value, Text:l.input[l.start:l.pos]}
+
+func (l *Lexer) endOfFile() {
+	l.tokens <- Token{Value: eof}
+}
+
+func (l *Lexer) lexInt() {
+	for rune := l.next(); unicode.IsDigit(rune); rune = l.next() {
+	}
+	l.backup()
+	l.emit(T_INT)
+}
+
+func (l *Lexer) emitSingle(rune int) bool {
+	if unicode.IsDigit(rune) {
+		l.backup()
+		l.lexInt()
+	}
+	switch rune {
+	case '+':
+		l.emit(T_PLUS)
+		return true
+	case '-':
+		l.emit(T_MINUS)
+		return true
+	case '(':
+		l.emit(T_LPAREN)
+		return true
+	case ')':
+		l.emit(T_RPAREN)
+		return true
+	default:
+
+	}
+	return false
+}
+
+func (l *Lexer) emit(value int) {
+	l.tokens <- Token{Value: value, Text: l.input[l.start:l.pos]}
 	l.start = l.pos
 }
 func (l *Lexer) next() (rune int) {
-	if (l.pos > len(l.input)) {
+	if l.pos >= len(l.input) {
 		l.width = 0
-		return eof;
+		return eof
 	}
-	return 0
+	rune, l.width = utf8.DecodeRuneInString(l.input[l.pos:])
+	l.pos += l.width
+	return rune
+}
+func (l *Lexer) backup() {
+	l.pos -= l.width
 }
 func defaultState(l *Lexer) stateFn {
 	return defaultState
 }
-
-
-
-
-
-
-
-
--- lex/lex_test.go
+++ lex/lex_test.go
@@ -2,9 +2,57 @@ package lex
 
 import "testing"
 
-func Test_EmptyShouldReturnEOF(t *testing.T){
+func Test_EmptyShouldReturnEOF(t *testing.T) {
 	_, ch := NewLexer("")
 	if tok := <-ch; tok.Value != eof {
-		t.Error("An empty lexer should only return EOF")
+		t.Errorf("An empty lexer should only return EOF, got %v instead", tok.Value)
+	}
+}
+
+func Test_SymbolShouldReturnTokenValue(t *testing.T) {
+	syms := []string{
+		"+",
+		"-",
+		"(",
+		")"}
+	expected := []int{
+		T_PLUS,
+		T_MINUS,
+		T_LPAREN,
+		T_RPAREN}
+	for i := range syms {
+		_, ch := NewLexer(syms[i])
+		if tok := <-ch; tok.Value != expected[i] {
+			t.Errorf("for string %v, expected %v, got %v", syms[i], expected[i], tok.Value)
+		}
+	}
+
+}
+func Test_MultipleSymbolsShouldEmitMultipleTokens(t *testing.T) {
+	syms := "+-()"
+	expected := []int{
+		T_PLUS,
+		T_MINUS,
+		T_LPAREN,
+		T_RPAREN,
+		eof}
+
+	_, ch := NewLexer(syms)
+	i := 0
+	for tok := range ch {
+		if tok.Value != expected[i] {
+			t.Errorf("for string %c, expected %v, got %v", syms[i], expected[i], tok.Value)
+		}
+		i++
+	}
+}
+
+func Test_ShouldLexInts(t *testing.T) {
+	text := "123"
+	expected := T_INT
+
+	_, ch := NewLexer(text)
+	if tok := <-ch; tok.Value != expected || tok.Text != text {
+		t.Errorf("for string %v got %v(%v), expected %v", text, tok.Value, tok.Text, expected)
 	}
 }
