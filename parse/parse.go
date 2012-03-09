eec320c4c52e7a59612b804cb51d3366b8a2304f
Max Polun <orrerey@gmail.com>
2012-03-09 15:27:06 -0500

    Basic parsing is working now

--- parse/parse.go
+++ parse/parse.go
@@ -1,12 +1,96 @@
 package parse
 
 import (
-	"../lex"
+	"fmt"
+	"github.com/maxpolun/tundra/lex"
+	"strconv"
+	"strings"
 )
 
-func Main(str string) {
-	l, ch := lex.NewLexer(str)
-	for tok := range ch {
+type Parser struct {
+	ch chan lex.Token
+}
 
+type ASTNode interface {
+	Eval() int
+}
+type IntLiteral int
+
+func (i IntLiteral) Eval() int {
+	return int(i)
+}
+func (i IntLiteral) String() string {
+	return fmt.Sprint(int(i))
+}
+
+type BinOp struct {
+	Left  ASTNode
+	Right ASTNode
+	Op    func(int, int) int
+	text  string
+}
+
+func (b *BinOp) Eval() int {
+	return b.Op(b.Left.Eval(), b.Right.Eval())
+}
+func (b *BinOp) String() string {
+	return fmt.Sprintf("%v(%v, %v)", b.text, b.Left, b.Right)
+}
+
+func NewParser(c chan lex.Token) Parser {
+	p := Parser{ch: c}
+	return p
+}
+
+var binopTable = map[string]func(int, int) int{
+	"+": func(a, b int) int {
+		return a + b
+	},
+	"-": func(a, b int) int {
+		return a - b
+	}}
+
+func parseLit(tok lex.Token) ASTNode {
+	switch tok.Value {
+	case lex.T_INT:
+		num, _ := strconv.Atoi(tok.Text)
+		return IntLiteral(num)
+	default:
+		return nil
 	}
+	return nil
+}
+
+func strTokList(tokList []lex.Token) string {
+	stringList := make([]string, len(tokList))
+	for i := range tokList {
+		stringList[i] = tokList[i].Text
+	}
+	return strings.Join(stringList, ", ")
+}
+
+func (p *Parser) Parse() ASTNode {
+
+	tokList := make([]lex.Token, 0, 10)
+
+	for tok := range p.ch {
+		tokList = append(tokList, tok)
+	}
+	fmt.Printf("number of tokens to parse: %v\n", len(tokList))
+	fmt.Printf("tokens: %v\n", strTokList(tokList))
+	if len(tokList) == 0 {
+		return nil
+	}
+	if len(tokList) == 1 {
+		return parseLit(tokList[0])
+	}
+	if tokList[1].Value == lex.T_PLUS {
+		return &BinOp{Left: parseLit(tokList[0]),
+			Right: parseLit(tokList[2]),
+			Op:    binopTable["+"],
+			text:  tokList[1].Text}
+	} else {
+		return parseLit(tokList[0])
+	}
+	return nil
 }
--- parse/parse_test.go
+++ parse/parse_test.go
@@ -1,5 +1,62 @@
 package parse
 
 import (
+	"github.com/maxpolun/tundra/lex"
 	"testing"
 )
+
+func Test_ShouldReturnNilJustEOF(t *testing.T) {
+	_, ch := lex.NewLexer("")
+	p := NewParser(ch)
+	if tree := p.Parse(); tree != nil {
+		t.Errorf("Parse on empty string should only return nil, got %v instead", tree)
+	}
+}
+
+func Test_IntegerShouldReturnIntegerLiterals(t *testing.T) {
+	_, ch := lex.NewLexer("123")
+	p := NewParser(ch)
+	if tree := p.Parse(); tree.Eval() != 123 {
+		t.Errorf("Parse Expected to produce %v, got %v", 123, tree.Eval())
+	}
+}
+
+func makeFakeChan(tokens []lex.Token) (l *lex.Lexer, ch chan lex.Token) {
+	ch = make(chan lex.Token)
+	go func() {
+		for _, t := range tokens {
+			ch <- t
+		}
+		close(ch)
+	}()
+	return nil, ch
+}
+
+func Test_PlusMinusShouldParse(t *testing.T) {
+	tokens := [][]lex.Token{
+		[]lex.Token{
+			lex.Token{lex.T_INT, "1"}, lex.Token{lex.T_PLUS, "+"}, lex.Token{lex.T_INT, "1"}}}
+
+	expected := []int{
+		2}
+	for index := range tokens {
+		_, ch := makeFakeChan(tokens[index])
+		p := NewParser(ch)
+		if tree := p.Parse(); tree.Eval() != expected[index] {
+			t.Errorf("Eval() produced %v, expected %v", tree.Eval(), expected[index])
+		}
+	}
+}
+func Test_PlusShouldEval(t *testing.T) {
+	tree := &BinOp{
+		Left:  IntLiteral(1),
+		Right: IntLiteral(1),
+		Op: func(a, b int) int {
+			return a + b
+		},
+		text: "+"}
+	result := tree.Eval()
+	if result != 2 {
+		t.Errorf("%v.Eval() produced %v should produce 2", tree, result)
+	}
+}
